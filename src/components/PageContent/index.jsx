b08154c4cf7fb2196e63ebfb77a017705dac96ec
cado19 <mryoungrichie@gmail.com>
2025-02-12 22:41:47 +0300

    layout file

--- src/components/PageContent/index.jsx
+++ src/components/PageContent/index.jsx
@@ -1,18 +1,31 @@
-import React from 'react'
-import Sidebar from './Sidebar'
-import Header from './Header'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { Outlet } from "react-router-dom";
+import { motion } from 'framer-motion'
 
 const PageContent = () => {
-  return (
-    <div className='flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden'>
-        <Sidebar />
-        <div className='flex-1'>
-          <div className='bg-teal-200'><Header /></div>
-          <Outlet />
-        </div>
-    </div>
-  )
-}
+  const containerVariants = {
+    hidden: { opacity: 0, y: -50 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.85} },
+  };
 
-export default PageContent
+  return (
+    <motion.div
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row bg-neutral-200 h-screen w-screen overflow-hidden"
+    >
+      <Sidebar />
+      <div className="flex-1 overflow-auto">
+        <div className="bg-teal-200">
+          <Header />
+        </div>
+        <Outlet />
+      </div>
+    </motion.div>
+  );
+};
+
+export default PageContent;
