003b9e18c273df6cd341db0b7e50d7c692dba416
cado19 <mryoungrichie@gmail.com>
2025-02-12 22:42:12 +0300

    top header

--- src/components/PageContent/Header.jsx
+++ src/components/PageContent/Header.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <div>
-      Header
+    <div className='bg-white h-16 px-4 flex justify-between items-center border-b border-gray-200'>
+      
     </div>
   )
 }
