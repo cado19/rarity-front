99be4705ec80f61a8c98b076abefa93fd1f8119e
cado19 <mryoungrichie@gmail.com>
2025-02-12 22:39:25 +0300

    sidebar with links

--- src/components/PageContent/Sidebar.jsx
+++ src/components/PageContent/Sidebar.jsx
@@ -1,25 +1,34 @@
-// THIS COMPONENT RENDERS THE ENTIRE SIDEBAR. IT IMPORTS LINKS AND PASSES THEM TO SIDEBAR LINK COMPONENT FOR FORMATTING 
-import React from 'react'
+// THIS COMPONENT RENDERS THE ENTIRE SIDEBAR. IT IMPORTS LINKS AND PASSES THEM TO SIDEBAR LINK COMPONENT FOR FORMATTING
+import React, { useState } from "react";
 import { FcBullish } from "react-icons/fc";
-import { DASHBOARD_SIDEBAR_BOTTOM_LINKS, DASHBOARD_SIDEBAR_LINKS } from '../../constants/navigation';
-import SidebarLink from './SidebarLink';
-
-
+import {
+  DASHBOARD_SIDEBAR_BOTTOM_LINKS,
+  DASHBOARD_SIDEBAR_LINKS,
+} from "../../constants/navigation";
+import SidebarLink from "./SidebarLink";
+import { motion } from "motion/react";
 
 const Sidebar = () => {
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
   return (
-    <div className='bg-neutral-900 w-60 p-3 flex flex-col text-white'>
-      
+    <motion.div
+      className={`bg-neutral-900 p-3 backdrop-blur-md bg-opacity-90 transition-all duration-300 ease-in-out flex-shrink-0 flex flex-col text-white ${
+        isSideBarOpen ? "w-64" : "w-20"
+      }`}
+      animate={{ width: isSideBarOpen ? 256 : 80 }}
+      onHoverStart={() => setIsSideBarOpen(true)}
+      onHoverEnd={() => setIsSideBarOpen(false)}
+    >
       {/* Brand and Logo  */}
       <div className="flex items-center gap-2 px-1 py-3">
         <FcBullish fontSize={24} />
-        <span className='text-neutral-100 text-lg '>OpenShop</span>
+        <span className="text-neutral-100 text-lg ">Rarity Cars</span>
       </div>
 
       {/* Links  */}
-      <div className='flex-1 py-8 flex flex-col gap-0.5'>
+      <div className="flex-1 py-8 flex flex-col gap-0.5">
         {DASHBOARD_SIDEBAR_LINKS.map((item) => (
-          <SidebarLink key={item.key} item={item} />
+          <SidebarLink key={item.key} item={item} isSideBarOpen={isSideBarOpen} />
         ))}
       </div>
 
@@ -29,8 +38,8 @@ const Sidebar = () => {
           <SidebarLink key={item.key} item={item} />
         ))}
       </div>
-    </div>
-  )
-}
+    </motion.div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/components/PageContent/SidebarLink.jsx
+++ src/components/PageContent/SidebarLink.jsx
@@ -1,14 +1,31 @@
-import React from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from "motion/react";
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
 
-export default function SidebarLink({ item }) {
-    const { pathname } = useLocation()
-    const linkClasses = `flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base ${pathname == item.path ? 'bg-neutral-700 text-white': 'text-neutral-400'}`
+export default function SidebarLink({ item, isSideBarOpen }) {
+  const { pathname } = useLocation();
+  const linkClasses = `flex items-center gap-2 font-light px-3 py-2 hover:bg-neutral-700 hover:no-underline active:bg-neutral-600 rounded-sm text-base ${
+    pathname == item.path ? "bg-neutral-700 text-white" : "text-neutral-400"
+  }`;
 
-    return (
+  return (
+    <motion.div>
       <Link to={item.path} className={linkClasses}>
-        <span className='text-xl'>{ item.icon }</span>
-        { item.label }
+        <span className="text-xl min-w-5">{item.icon}</span>
+        <AnimatePresence>
+          {isSideBarOpen && (
+            <motion.span
+              className="ml-4 whitespace-nowrap"
+              initial={{ opacity: 0, width: 0 }}
+              animate={{opacity: 1, width: "auto"}}
+              exit={{ opacity: 0, width: 0 }}
+              transition={{ duration: 0.2, delay: 0.3 }}
+            >
+              {item.label}
+            </motion.span>
+          )}
+        </AnimatePresence>
       </Link>
-    )
+    </motion.div>
+  );
 }
