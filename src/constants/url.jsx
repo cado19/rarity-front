cb80322d0904fec2c938b45bc37f49f39bdda780
cado19 <mryoungrichie@gmail.com>
2025-02-12 22:41:22 +0300

    url for external API and also Front End URL

--- src/constants/url.jsx
+++ src/constants/url.jsx
@@ -1,1 +1,5 @@
-export const baseURL = ' http://localhost:3000';
+export const baseURL = 'http://localhost:3000';
+// export const baseURL = 'http://167.172.62.134';
+// export const voucherUrl = 'raritycars.com/booking_voucher/2';
+export const voucherUrl = 'http:/localhost:5173/voucher';
+export const feUrl = 'http://localhost:5173'; // URL for our front end app
