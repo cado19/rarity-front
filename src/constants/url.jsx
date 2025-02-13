85e72db0f13aabbe8517f5ed849cd27025d3cc2b
cado19 <mryoungrichie@gmail.com>
2025-02-13 11:09:21 +0300

    changed base, front end and voucher urls

--- src/constants/url.jsx
+++ src/constants/url.jsx
@@ -1,5 +1,6 @@
-export const baseURL = 'http://localhost:3000';
-// export const baseURL = 'http://167.172.62.134';
+// export const baseURL = 'http://localhost:3000';
+export const baseURL = 'http://167.172.62.134';
 // export const voucherUrl = 'raritycars.com/booking_voucher/2';
-export const voucherUrl = 'http:/localhost:5173/voucher';
-export const feUrl = 'http://localhost:5173'; // URL for our front end app
+// export const voucherUrl = 'http:/localhost:5173/voucher';
+export const voucherUrl = 'https://www.raritycars.com/voucher';
+export const feUrl = 'https://www.raritycars.com'; // URL for our front end app
