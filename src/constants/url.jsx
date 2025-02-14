597d8ad1998e8fae3c40c35f55710c55c9fab7dd
cado19 <mryoungrichie@gmail.com>
2025-02-14 12:54:03 +0300

    modified baseurl with https

--- src/constants/url.jsx
+++ src/constants/url.jsx
@@ -1,5 +1,5 @@
 // export const baseURL = 'http://localhost:3000';
-export const baseURL = 'test.jinamizi.dev';
+export const baseURL = 'https://test.jinamizi.dev';
 // export const voucherUrl = 'raritycars.com/booking_voucher/2';
 // export const voucherUrl = 'http:/localhost:5173/voucher';
 export const voucherUrl = 'https://www.raritycars.com/voucher';
