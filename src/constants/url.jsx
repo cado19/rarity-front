bca0ae12f6e4d3fc86af8bf74778a99df2a83e90
cado19 <mryoungrichie@gmail.com>
2025-02-21 17:06:38 +0300

    changed url to public

--- src/constants/url.jsx
+++ src/constants/url.jsx
@@ -4,3 +4,6 @@ export const baseURL = 'https://test.jinamizi.dev';
 // export const voucherUrl = 'http:/localhost:5173/voucher';
 export const voucherUrl = 'https://www.raritycars.com/voucher';
 export const feUrl = 'https://www.raritycars.com'; // URL for our front end app
+
+export const contractUrl = 'https://client.raritycars.com/index.php?page=contract'; // base url that is used for retrieving contracts and viewing vouchers
+export const userUrl = 'https://client.raritycars.com/index.php?page=client'; // base url that is used for client self register and viewing vouchers
