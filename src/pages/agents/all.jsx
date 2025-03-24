76c4c988a656cd2e1bd758eeecbe3d726a298b4b
cado19 <mryoungrichie@gmail.com>
2025-03-24 20:00:33 +0300

    show an error in event there are no more drivers

--- src/pages/agents/all.jsx
+++ src/pages/agents/all.jsx
@@ -10,143 +10,146 @@ import DriverNav from "../../components/navs/drivernav";
 import AgentNav from "../../components/navs/agentnav";
 
 export default function AllAgents() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-      const columns = [
-        {
-          name: "Name",
-          selector: (row) => row.name,
-          sortable: true,
-        },
-        {
-          name: "Email",
-          selector: (row) => row.email,
-        },
-        {
-          name: "Phone Number",
-          selector: (row) => row.phone_no,
-          sortable: true,
-        },
-        {
-          name: "Country",
-          selector: (row) => row.country,
-          sortable: true,
-        },
-        {
-          name: "Options",
-          cell: (row) => <Link to={`/agent/${row.id}`}>Details</Link>,
-        },
+  const columns = [
+    {
+      name: "Name",
+      selector: (row) => row.name,
+      sortable: true,
+    },
+    {
+      name: "Email",
+      selector: (row) => row.email,
+    },
+    {
+      name: "Phone Number",
+      selector: (row) => row.phone_no,
+      sortable: true,
+    },
+    {
+      name: "Country",
+      selector: (row) => row.country,
+      sortable: true,
+    },
+    {
+      name: "Options",
+      cell: (row) => <Link to={`/agent/${row.id}`}>Details</Link>,
+    },
+  ];
 
-      ];
+  const agentData = [];
 
-      const agentData = [];
+  const addAgentData = (agent) => {
+    agentData.push({
+      id: agent.id,
+      name: agent.name,
+      email: agent.email,
+      phone_no: agent.phone_no,
+      country: agent.country,
+    });
+  };
 
-      const addAgentData = (agent) => {
-        agentData.push({
-          id: agent.id,
-          name: agent.name,
-          email: agent.email,
-          phone_no: agent.phone_no,
-          country: agent.country,
-        });
-      };
+  const [agents, setAgents] = useState(agentData);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-    const [agents, setAgents] = useState(agentData);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(null);
-    
-    const agentURL = baseURL + "/api/agents/all.php";
+  const agentURL = baseURL + "/api/agents/all.php";
 
-    const handleSearch = (e) => {
-        let searchValue;
-        let nameValue;
-        let emailValue;
-        let countryValue;
-        let phoneNoValue;
-        // let eyeColorValue;
-    
-        const newRows = agentData.filter((row) => {
-          nameValue = row.name
-            .toString()
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-    
-          emailValue = row.email
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-    
-          countryValue = row.country
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-    
-          phoneNoValue = row.phone_no
-            .toLowerCase()
-            .includes(e.target.value.toLowerCase());
-    
-          if (nameValue) {
-            searchValue = nameValue;
-          } else if (emailValue) {
-            searchValue = emailValue;
-          } else if (countryValue) {
-            searchValue = countryValue;
-          } else {
-            searchValue = phoneNoValue;
-          }
-    
-          return searchValue;
-        });
-    
-        setAgents(newRows);
-    
-        // let query = e.target.value;
-        // const newRecords = agentData.filter((item) =>
-        //   item.first_name.toLocaleLowerCase().includes(query.toLocaleLowerCase()) 
-        // );
-        // setAgents(newRecords);
-      };
+  const handleSearch = (e) => {
+    let searchValue;
+    let nameValue;
+    let emailValue;
+    let countryValue;
+    let phoneNoValue;
+    // let eyeColorValue;
 
-      const getAgents = async () => {
-        try {
-          await axios.get(agentURL).then((response) => {
-            console.log(response);
-            response.data.agents.forEach((agent) => addAgentData(agent));
-            // setAgents(response.data.data);
-            console.log(agentData);
-            setLoading(false);
-          });
-        } catch (error) {
-          const errorMessage = "Error: " + error.message;
-          setError(errorMessage);
+    const newRows = agentData.filter((row) => {
+      nameValue = row.name
+        .toString()
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+
+      emailValue = row.email
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+
+      countryValue = row.country
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+
+      phoneNoValue = row.phone_no
+        .toLowerCase()
+        .includes(e.target.value.toLowerCase());
+
+      if (nameValue) {
+        searchValue = nameValue;
+      } else if (emailValue) {
+        searchValue = emailValue;
+      } else if (countryValue) {
+        searchValue = countryValue;
+      } else {
+        searchValue = phoneNoValue;
+      }
+
+      return searchValue;
+    });
+
+    setAgents(newRows);
+
+    // let query = e.target.value;
+    // const newRecords = agentData.filter((item) =>
+    //   item.first_name.toLocaleLowerCase().includes(query.toLocaleLowerCase())
+    // );
+    // setAgents(newRecords);
+  };
+
+  const getAgents = async () => {
+    try {
+      await axios.get(agentURL).then((response) => {
+        console.log(response);
+        if (response.data.agents.length === 0) {
+          setError("No agents found");
+        } else {
+          response.data.agents.forEach((agent) => addAgentData(agent));
+          // setAgents(response.data.data);
+          console.log(agentData);
+          setLoading(false);
         }
-      };
+      });
+    } catch (error) {
+      const errorMessage = "Error: " + error.message;
+      setError(errorMessage);
+    }
+  };
 
-        useEffect(() => {
-          const loggedIn = localStorage.getItem("loggedIn");
-          if (!loggedIn){
-            navigate("/login");
-          }
-          getAgents();
-        }, [agents, navigate]);
-      
-        if (error) {
-          return (
-            <div className="bg-white px-4 pb-4 rounded border-gray-200 flex-1 shadow-md">
-              {error}
-            </div>
-          );
-        }
-        if (loading) {
-          return (
-            <div className="bg-white px-4 pb-4 rounded border-gray-200 flex-1 shadow-md">
-              <Loading />
-            </div>
-          );
-        }
-      
+  useEffect(() => {
+    const loggedIn = localStorage.getItem("loggedIn");
+    if (!loggedIn) {
+      navigate("/login");
+    }
+    getAgents();
+  }, [agents, navigate]);
+
+  if (error) {
+    return (
+      <div className="bg-white px-4 pb-4 rounded border-gray-200 flex-1 shadow-md">
+        {error}
+      </div>
+    );
+  }
+  if (loading) {
+    return (
+      <div className="bg-white px-4 pb-4 rounded border-gray-200 flex-1 shadow-md">
+        <Loading />
+      </div>
+    );
+  }
+
   return (
     <div className="bg-white px-4 pb-4 rounded border-gray-200 flex-1 shadow-md mt-2 mx-3">
-    <div className="flex justify-end">
-      {/* <input
+      <div className="flex justify-end">
+        {/* <input
         type="text"
         placeholder="Search"
         className="appearance-none bg-transparent border-none text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
@@ -158,44 +161,44 @@ export default function AllAgents() {
       >
         Search
       </label> */}
-      <div class="relative">
-        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
-          <svg
-            className="w-4 h-4 text-gray-500 dark:text-gray-400"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 20 20"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-            />
-          </svg>
+        <div class="relative">
+          <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 20"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+              />
+            </svg>
+          </div>
+          <input
+            type="search"
+            id="default-search"
+            className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Search Agents..."
+            onChange={handleSearch}
+          />
         </div>
-        <input
-          type="search"
-          id="default-search"
-          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="Search Agents..."
-          onChange={handleSearch}
-        />
       </div>
+      <AgentNav />
+      {/* <h1 className="text-bold text-center">Vehicles </h1> */}
+      <DataTable
+        columns={columns}
+        data={agents}
+        pagination
+        title="Agents"
+        highlightOnHover
+        progressPending={loading}
+        persistTableHead
+      />
     </div>
-    <AgentNav />
-    {/* <h1 className="text-bold text-center">Vehicles </h1> */}
-    <DataTable
-      columns={columns}
-      data={agents}
-      pagination
-      title="Agents"
-      highlightOnHover
-      progressPending={loading}
-      persistTableHead
-    />
-  </div>
-  )
+  );
 }
